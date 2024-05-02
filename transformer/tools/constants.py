b848eab080a2bd219c3e4c53347181dd532e4c97
Stan Vertessen <>
2024-05-02 20:47:24 +0200

    Added uitvoerder_results.csv and updated main.py function

--- transformer/tools/constants.py
+++ transformer/tools/constants.py
@@ -9,6 +9,7 @@ ACTIVITEIT_TYPE = "activiteit"
 DEELNAME_TYPE = "deelname"
 PARTICIPANT_TYPE = "participant"
 ORGANISATOR_TYPE = "organisator"
+UITVOERDER_TYPE = "uitvoerder"
 
 # the output port name as declared in the data-product.yml used to store rdf data in fuseki
 FUSEKI_OUTPUT_PORT_NAME = "demosparql"
